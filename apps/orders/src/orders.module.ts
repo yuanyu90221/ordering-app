a90245739f29844792b02613b9a3500f144a4250
gson Liang <yuanyu90221@gmail.com>
2022-11-29 23:16:38 +0800

    [common] add mongoose setup

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
-
+import * as Joi from 'joi';
+import { DatabaseModule } from '@app/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/order.schema';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+    DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,2 +1,3 @@
-export * from './common.module';
-export * from './common.service';
+export * from './database/abstract.repository';
+export * from './database/abstract.schema';
+export * from './database/database.module';
