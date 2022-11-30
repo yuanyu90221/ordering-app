f7d8c93a6e6e329dd44084afd3aa1683d00b3899
gson Liang <yuanyu90221@gmail.com>
2022-12-01 03:10:13 +0800

    [billing] config rabbitmq connection setup

--- apps/billing/src/billing.module.ts
+++ apps/billing/src/billing.module.ts
@@ -1,9 +1,20 @@
+import { RmqModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { BillingController } from './billing.controller';
 import { BillingService } from './billing.service';
-
+import * as Joi from 'joi';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_BILLING_QUEUE: Joi.string().required(),
+      }),
+    }),
+    RmqModule,
+  ],
   controllers: [BillingController],
   providers: [BillingService],
 })
